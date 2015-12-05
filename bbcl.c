296b8164d45a1d1ce5fe457be4a1590485ae9fdd
Petr Vaněk <arkamar@atlas.cz>
2015-12-05 12:20:02 +0100

    find name

--- bbcl.c
+++ bbcl.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int
 main(int argc, char *argv[]) {
 	char * buff = NULL;
 	size_t size = 0;
+	char * ob;
+	char * name;
 	while (getline(&buff, &size, stdin) > 0) {
-		if (buff[18] == 'v' || buff[18] == '!')
-			printf("%s", buff);
+		if (size < 21)
+			continue;
+		switch (buff[18]) {
+		case '!':
+			name = buff + 21;
+			ob = strchr(name, '(');
+			if (!ob)
+				continue;
+			ob[0] = '\0';
+			break;
+		case 'v':
+			name = buff + 19;
+			ob = strchr(name, '\n');
+			if (ob)
+				ob[0] = '\0';
+			break;
+		default:
+			continue;
+		}
+		printf("%s\n", name);
 	}
 	free(buff);
 	return 0;
